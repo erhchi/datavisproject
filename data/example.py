895feed1757dc60c1c75726675aa38f251d8b437
Josh Yueh <32748971+tyueh@users.noreply.github.com>
2019-05-26 13:50:50 -0500

    Add files via upload

---
+++ data/example.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for,request
+from flask import make_response
+from flask import jsonify
+
+#...other code...
+
+@app.route('/background_process')
+def background_process():
+	try:
+		lang = request.args.get('proglang', 0, type=str)
+		if lang.lower() == 'python':
+			return jsonify(result='You are wise')
+		else:
+			return jsonify(result='Try again.')
+	except Exception as e:
+		return str(e)
---
+++ data/fake_career.js
@@ -0,0 +1,11 @@
+var career_csv_data = `label,value
+Information Security Analysts,10
+Computer and Information Research Scientists,4
+Web Developers,12
+Computer Systems Analysts,5
+Software Developers,4
+Computer Network Architects,1
+Database Administrators,8
+Software Quality Assurance Engineers and Testers,9
+Database Architects,7
+Computer Programmers,3`
