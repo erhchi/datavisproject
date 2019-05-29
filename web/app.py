9598f8b94617778b6b52dc286664c3688796d62f
erichaerr@outlook.com <erichaerr@outlook.com>
2019-05-29 12:37:53 -0500

    added caching to loading of csv data

--- web/app.py
+++ web/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for, jsonify
-
+from flask import render_template, request, jsonify
 from web.static.data.calculate_sim import *
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/index')
@@ -15,25 +13,13 @@ def main():
 def selection():
     data = request.get_json();
     course_idx = data['a']
-    print(course_idx)
 
     job_idx = get_job_sim(course_idx)
-
     job_idx_by_groups = get_group_sim(course_idx)
-
-    # with open('../web/static/data/index_by_sim.js', 'w') as outfile:
-    #     outfile.write("var course_idx = ")
-    #     json.dump(course_idx, outfile)
-    #     outfile.write("\nvar job_idx = ")
-    #     json.dump(job_idx, outfile)
-    #     outfile.write("\nvar job_idx_by_groups = ")
-    #     json.dump(job_idx_by_groups, outfile)
-    # outfile.close()
 
     toReturn = {'course_idx' : course_idx, 'job_idx' : job_idx, 'job_idx_by_groups' : job_idx_by_groups}
     return jsonify(toReturn)
     #return render_template('index.html')
-
 
 
 
--- web/static/data/calculate_sim.py
+++ web/static/data/calculate_sim.py
@@ -6,12 +6,42 @@ import os
 import json
 from collections import defaultdict
 
+from werkzeug.contrib.cache import SimpleCache
+cache = SimpleCache()
+
+
 input_idx = [34,21,41,88,81,12,121,141]
+
+def open_courses():
+    course_data = cache.get('course_data')
+    if course_data is None:
+        fn = os.path.join(os.path.dirname(__file__), 'course_mat.csv')
+        cache.set('course_data', np.array(pd.read_csv(fn)))
+        course_data = cache.get('course_data')
+    return course_data
+
+
+def open_jobs():
+    jobs_data = cache.get('jobs_data')
+    if jobs_data is None:
+        fn = os.path.join(os.path.dirname(__file__), 'job_mat.csv')
+        cache.set('jobs_data', np.array(pd.read_csv(fn)))
+        jobs_data = cache.get('jobs_data')
+    return jobs_data
+
+def open_concentrations():
+    concentration_data = cache.get('concentration_data')
+    if concentration_data is None:
+        fn = os.path.join(os.path.dirname(__file__), 'courses.csv')
+        cache.set('concentration_data', pd.read_csv(fn))
+        concentration_data = cache.get('concentration_data')
+    return concentration_data
 
 
 def get_course_idx_by_concentration(idx):
-    fn = os.path.join(os.path.dirname(__file__), 'courses.csv')
-    c_df = pd.read_csv(fn)
+
+    c_df = open_concentrations()
+
     res = {level:[] for level in set(c_df.level.tolist())}
     for i in idx:
         res[c_df.iloc[i].level].append(i)
@@ -24,15 +54,10 @@ def cosSim(inA,inB):
     return num / denom
 
 def get_job_sim(idx):
-    ## Load the Course matrix
-    fn = os.path.join(os.path.dirname(__file__), 'course_mat.csv')
-    c_mat = np.array(pd.read_csv(fn))
-    #c_mat = np.array(pd.read_csv('static/data/course_mat.csv'))
 
-    ## Load the Job matrix
-    fn = os.path.join(os.path.dirname(__file__), 'job_mat.csv')
-    j_mat = np.array(pd.read_csv(fn))
-    #j_mat = np.array(pd.read_csv('../data/job_mat.csv'))
+    c_mat = open_courses()
+    j_mat = open_jobs()
+
 
     ## normalize the Job matrix
     j_mat = minmax_scale(j_mat, feature_range=(0,1), axis=0)
@@ -49,8 +74,9 @@ def get_job_sim(idx):
     ## Run selected course on every job to find similarities
     sims = [cosSim(selected,j_mat[i]) for i in range(j_mat.shape[0])]
     adjusted_sims = np.argsort(sims)
-    ## print(adjusted_sims)
+
     return adjusted_sims.tolist()
+
 
 def get_group_sim(idx):
     dic = get_course_idx_by_concentration(idx)
@@ -67,14 +93,3 @@ sorted_job_idx = get_job_sim(input_idx)
 ## get sorted job index for all selected concentration
 group_sim_idxs = get_group_sim(input_idx)
 
-
-
-## write the index to a to js file
-# with open('index_by_sim.js', 'w') as outfile:
-#     outfile.write("var course_idx = ")
-#     json.dump(input_idx, outfile)
-#     outfile.write("\nvar job_idx = ")
-#     json.dump(sorted_job_idx, outfile)
-#     outfile.write("\nvar job_idx_by_groups = ")
-#     json.dump(group_sim_idxs, outfile)
-# outfile.close()
